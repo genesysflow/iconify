35f04e5c5f3fc6ec2743b8d92bb52260d3c866f5
Robert Andronache <robert.andronache@realpage.com>
2024-07-29 12:31:26 +0300

    Improved icon package generator

--- pkg/generator/generator.go
+++ pkg/generator/generator.go
@@ -10,15 +10,26 @@ import (
 	"github.com/iancoleman/strcase"
 )
 
-func Generate(api string) {
-	collections, err := iconify.GetCollections(api)
-	if err != nil {
-		return
-	}
+type IconifyGenerator struct {
+	API string
+	CWD string
+}
 
+// walk through the collections and generate the icons
+func Generate(api string) {
 	cwd, err := os.Getwd()
 	if err != nil {
 		log.Fatal(err)
+	}
+
+	generator := IconifyGenerator{
+		API: api,
+		CWD: cwd,
+	}
+
+	collections, err := iconify.GetCollections(api)
+	if err != nil {
+		return
 	}
 
 	for _, collection := range collections {
@@ -27,46 +38,45 @@ func Generate(api string) {
 			return
 		}
 
-		filePath, err := generateIconPackage(cwd, iconCollection.Prefix)
-		if err != nil {
-			log.Fatal("Error generating icon package: ", err)
-			return
-		}
-
-		file, err := os.OpenFile(filePath, os.O_APPEND|os.O_WRONLY, 0644)
-		if err != nil {
-			log.Fatal("Error opening package file: ", err)
-			return
-		}
-		defer file.Close()
-
 		for key, value := range iconCollection.Categories {
-			fmt.Println(key)
-			for _, icon := range value {
-				body, err := iconify.GetIcon(api, iconCollection.Prefix, icon)
-				if err != nil {
-					return
-				}
-				generateIcon(file, cwd, icon, body)
-			}
+			generator.generateFromCategory(key, iconCollection.Prefix, value)
 		}
 
-		for _, icon := range iconCollection.Uncategorized {
-			body, err := iconify.GetIcon(api, iconCollection.Prefix, icon)
-			if err != nil {
-				return
-			}
-			generateIcon(file, cwd, icon, body)
+		generator.generateFromCategory("Uncategorized", iconCollection.Prefix, iconCollection.Uncategorized)
+	}
+}
+
+// generate the icons from a category
+func (i *IconifyGenerator) generateFromCategory(category, prefix string, icons []string) {
+	filePath, err := i.generateIconPackage(prefix, category)
+	if err != nil {
+		log.Fatal("Error generating icon package: ", err)
+		return
+	}
+
+	file, err := os.OpenFile(filePath, os.O_APPEND|os.O_WRONLY, 0644)
+	if err != nil {
+		log.Fatal("Error opening package file: ", err)
+		return
+	}
+	defer file.Close()
+
+	for _, icon := range icons {
+		body, err := iconify.GetIcon(i.API, prefix, icon)
+		if err != nil {
+			return
 		}
+		i.generateIcon(file, icon, body)
 	}
 }
 
 var iconFunctionTemplate = ""
 
-func generateIcon(file *os.File, cwd, icon, body string) {
+// add an icon function to the file
+func (i *IconifyGenerator) generateIcon(file *os.File, icon, body string) {
 	functionName := getIconFunctionName(icon)
 	if iconFunctionTemplate == "" {
-		data, err := os.ReadFile(cwd + "/pkg/generator/icon_function.tpl.txt")
+		data, err := os.ReadFile(i.CWD + "/pkg/generator/icon_function.tpl.txt")
 		if err != nil {
 			log.Fatal(err)
 			return
@@ -79,16 +89,17 @@ func generateIcon(file *os.File, cwd, icon, body string) {
 	file.WriteString(iconFunction)
 }
 
-func generateIconPackage(cwd, prefix string) (string, error) {
-	data, err := os.ReadFile(cwd + "/pkg/generator/icon_package.tpl.txt")
+// create a file to store the icon functions
+func (i *IconifyGenerator) generateIconPackage(prefix, category string) (string, error) {
+	data, err := os.ReadFile(i.CWD + "/pkg/generator/icon_package.tpl.txt")
 	if err != nil {
 		return "", err
 	}
 
-	goFileName := "" + strcase.ToSnake(prefix) + ".templ"
+	goFileName := cleanCategoryName(category) + "_" + strcase.ToSnake(prefix) + ".templ"
 
 	packageTmpl := strings.ReplaceAll(string(data), "$PACKAGE-NAME$", strcase.ToSnake(prefix))
-	fileDir := cwd + "/" + strcase.ToSnake(prefix)
+	fileDir := i.CWD + "/" + strcase.ToSnake(prefix)
 	filePath := fileDir + "/" + goFileName
 	fmt.Println("Generating package: ", fileDir)
 
@@ -108,4 +119,11 @@ func generateIconPackage(cwd, prefix string) (string, error) {
 
 func getIconFunctionName(icon string) string {
 	return "Icon" + strcase.ToCamel(icon)
+}
+
+func cleanCategoryName(category string) string {
+	r := strings.NewReplacer("/", "", "+", "")
+	return strcase.ToSnake(
+		r.Replace(category),
+	)
 }
